114e5fd57abf4fee706fa63cd78ea8aea244b5b1
johncovenant <ufotjohnamos@gmail.com>
2023-05-22 14:02:33 +0100

    import image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import mealsImage from '../../assets/header.png'
+
 const Header = props => {
     return (
         <>
-
+            <header>
+                <h1>CJFOODS</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img src={mealsImage} />
+            </div>
         </>
     )
 };

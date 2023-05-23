aa60dbbb05aedf63fbccf8d16f60c943cd67c5c6
johncovenant <ufotjohnamos@gmail.com>
2023-05-23 14:25:23 +0100

    Dummy meals

---
+++ src/components/Meals/AvailableMeals.jsx
@@ -0,0 +1,36 @@
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'White Rice and stew',
+    description: 'whole grained rice and delicious stew made from carefully picked tomatoes',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Afang Soup',
+    description: 'An AkwaIbom special delicacy',
+    price: 30.55,
+  },
+  {
+    id: 'm3',
+    name: 'Jollof Rice',
+    description: 'Just like the ones at owambe',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Chicken and chips',
+    description: 'properly oiled and fried chicken and delicious chips',
+    price: 18.99,
+  },
+  {
+     id: 'm5',
+    name: 'Egusi Soup and pounded yam',
+    description: 'delicious egusi soup. one owambe mummy said i should add it too',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {};
+
+export default AvailableMeals;

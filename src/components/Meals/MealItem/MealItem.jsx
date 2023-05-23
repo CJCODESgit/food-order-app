2af1865c28471976d3ce74f1b6e9615f351db0a5
johncovenant <ufotjohnamos@gmail.com>
2023-05-23 23:55:27 +0100

    gone further

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,12 +1,16 @@
 const MealItem = (props) => {
+
+    const price = `$${props.price.toFixed(2)}`;
+
     return (
         <li>
             <div>
                 <h3>{props.name}</h3>
+                 <div>{props.description}</div>
+                <div>{price}</div>
             </div>
-            <div>{props.description}</div>
-            <div>{props.price}</div>
         </li>
+        
     )
 }
 

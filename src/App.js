66e73bb5628b4ded4c1b339191dfd0704fa99432
johncovenant <ufotjohnamos@gmail.com>
2023-05-23 15:36:18 +0100

    rendered meals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }

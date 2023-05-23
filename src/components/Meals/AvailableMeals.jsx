4891ddd1e8c81860be232a4e442996b61f60bb7e
johncovenant <ufotjohnamos@gmail.com>
2023-05-23 15:21:58 +0100

    Meals. jsx

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,3 +1,5 @@
+import classes from './AvailableMeals.module.css'
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -32,10 +34,10 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map();
+    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
 
     return (
-        <section>
+        <section className={classes.meals}>
             <ul>
                {mealsList} 
             </ul>

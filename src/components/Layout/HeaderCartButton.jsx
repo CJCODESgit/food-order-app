36b0fa9caccc2377f556524c652e94b57837b0eb
johncovenant <ufotjohnamos@gmail.com>
2023-05-23 14:01:10 +0100

    moving on to meals

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -8,7 +8,7 @@ const HeaderCartButton = props => {
             <span className={classes.icon}>
                 <CartIcon />
             </span>
-            <span>Your Cart</span>
+            <span>Cart</span>
             <span className={classes.badge}>3</span>
         </button>
     )

1436b2fc417c54419edd07956a3368b6d7206e37
johncovenant <ufotjohnamos@gmail.com>
2023-05-24 14:32:11 +0100

    cart dummy done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,20 @@
 import classes from './Cart.module.css'
 
 const Cart = () => {
-  return (
+  const cartItems = <ul className={classes['cart-items']}> {[{id: 'c1', name: 'Afang', amount: 2, price: 30.55 }].map((item) => <li>{item.name}</li>)}; </ul>
 
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>30.55</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
   )
 }
 

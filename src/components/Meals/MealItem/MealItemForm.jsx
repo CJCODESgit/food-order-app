53137445a5c678157e05c1e99b6a16645c37db3c
johncovenant <ufotjohnamos@gmail.com>
2023-05-24 13:34:03 +0100

    accessibility for screen readers adjustment

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -5,7 +5,7 @@ const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
             <Input label="Amount" input={{
-                id: 'amount',
+                id: 'amount_' + props.id,
                 type: 'number',
                 min: '0',
                 max: '10',

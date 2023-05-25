8f516a7f4c55e29eccf8e8a33fecce43ac29886d
johncovenant <ufotjohnamos@gmail.com>
2023-05-25 15:45:36 +0100

    cart updated with wrapping with Modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,11 @@
+import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
 
 const Cart = () => {
   const cartItems = <ul className={classes['cart-items']}> {[{id: 'c1', name: 'Afang', amount: 2, price: 30.55 }].map((item) => <li>{item.name}</li>)}; </ul>
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -14,7 +15,7 @@ const Cart = () => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 

5743acd360a0f4eaa6014e2121acac2b5d04deb8
johncovenant <ufotjohnamos@gmail.com>
2023-05-25 23:07:39 +0100

    cart-context done

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-React.createContext({
-  
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {}
 });
+
+export default CartContext;

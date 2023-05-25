b8db8a646292edaf42661bb84817a85756129af5
johncovenant <ufotjohnamos@gmail.com>
2023-05-25 15:44:18 +0100

    modal done for now

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,8 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
 
-
-const Modal = () => {
+const Backdrop = (props) => {
   return (
-    
+    <div className={classes.backdrop} />
+  )
+}
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  )
+}
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+      {}
+    </>
   );
 };
 

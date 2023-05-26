0b9999a709ff0c7738239a46774bc4829ca2cb6e
johncovenant <ufotjohnamos@gmail.com>
2023-05-26 17:01:23 +0100

    start of friday

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+   const cartCtx = useContext(CartContext);
+
+   const numberOf
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>

7511dc721f261a7617bda74a2eb9939bfdbe4e24
johncovenant <ufotjohnamos@gmail.com>
2023-05-30 15:10:32 +0100

    render order button conditionally adding cartitems

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,6 +8,8 @@ const Cart = (props) => {
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems =( 
     <ul 
       className={classes['cart-items']}> 
@@ -25,7 +27,7 @@ const Cart = (props) => {
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}     onClick={props.onClose} >Close</button>
-        <button className={classes.button}>Order</button>
+       {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   )

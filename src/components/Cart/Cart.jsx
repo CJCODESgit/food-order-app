887cba80413e08e45ef6e14ae6b322dcc75b4840
johncovenant <ufotjohnamos@gmail.com>
2023-05-30 16:05:42 +0100

    style updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
 import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -10,11 +11,26 @@ const Cart = (props) => {
 
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = id => {
+
+  };
+
+  const cartItemAddHandler = item => {
+
+  };
+
   const cartItems =( 
     <ul 
       className={classes['cart-items']}> 
       {cartCtx.items.map((item) =>
-      <li>{item.name}</li>)} 
+      <CartItem 
+      key={item.id} 
+      name={item.name} 
+      amount={item.amount} 
+      price={item.price} 
+      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+      onAdd={cartItemAddHandler.bind(null,item)}
+      />)} 
     </ul>
      );
 

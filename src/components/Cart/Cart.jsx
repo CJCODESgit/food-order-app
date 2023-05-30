2d383821055d721714d93b791dbf29627f9664b7
johncovenant <ufotjohnamos@gmail.com>
2023-05-30 15:06:26 +0100

    fixed totalamount to be dynammic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,27 @@
+import { useContext } from 'react'
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
+import CartContext from '../../store/cart-context'
 
 const Cart = (props) => {
-  const cartItems = <ul className={classes['cart-items']}> {[{id: 'c1', name: 'Afang', amount: 2, price: 30.55 }].map((item) => <li>{item.name}</li>)} </ul>
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const cartItems =( 
+    <ul 
+      className={classes['cart-items']}> 
+      {cartCtx.items.map((item) =>
+      <li>{item.name}</li>)} 
+    </ul>
+     );
 
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>30.55</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}     onClick={props.onClose} >Close</button>

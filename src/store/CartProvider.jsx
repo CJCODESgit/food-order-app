1c5a2fd9ffd1bf2f7030ab544df9655a91996c79
johncovenant <ufotjohnamos@gmail.com>
2023-05-31 16:30:28 +0100

    idk what i'm cookin gyet but watch

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -10,8 +10,24 @@ const defaultCartState={
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItem;
+    let updatedItems;
+
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+    }
+
+    const updatedItems = state.items.concat(action.item);
+    
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount

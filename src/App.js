eb51c6d0973b3219daecfb135a63f57621f851b1
johncovenant <ufotjohnamos@gmail.com>
2023-06-01 13:52:28 +0100

    updated header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./components/Layout/Header";
+import Header from "./components/Header/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";

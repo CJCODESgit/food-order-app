254ba43b7a6e68b92ffe321a73f5cf08eef6d26c
johncovenant <ufotjohnamos@gmail.com>
2023-06-02 01:32:53 +0100

    applying usestate

--- src/components/Header/HeaderCartButton.jsx
+++ src/components/Header/HeaderCartButton.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
    const cartCtx = useContext(CartContext);
 
    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
    }, 0);
 
-   const btnClasses = `${classes.button} ${classes.bump}`;
+   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+   useEffect (() => {
+    setBtnIsHighlighted(true);
+   }, [])
 
     return (
         <button className={btnClasses} onClick={props.onClick}>

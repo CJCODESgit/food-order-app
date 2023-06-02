31d5056d05c5a5e07b23f5aa08329f077a74d2fe
johncovenant <ufotjohnamos@gmail.com>
2023-06-02 01:44:53 +0100

    done with the animation goodnight

--- src/components/Header/HeaderCartButton.jsx
+++ src/components/Header/HeaderCartButton.jsx
@@ -9,15 +9,30 @@ const HeaderCartButton = props => {
 
    const cartCtx = useContext(CartContext);
 
-   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+   const {items} = cartCtx;
+
+   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
    }, 0);
+
+   
 
    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
    useEffect (() => {
+    if (items.length === 0) {
+        return;
+    }
     setBtnIsHighlighted(true);
-   }, [])
+
+    const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer);
+    };
+   }, [items]);
 
     return (
         <button className={btnClasses} onClick={props.onClick}>
